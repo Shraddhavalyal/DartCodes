0a908626af4bcc2a1792477230cc17383256c354
Shraddhavalyal <145037969+Shraddhavalyal@users.noreply.github.com>
2024-09-27 11:46:25 +0530

    Update Program10.dart

--- Assignments/NestedForLoop02/Program10.dart
+++ Assignments/NestedForLoop02/Program10.dart
@@ -1,14 +1,15 @@
 import "dart:io";
 void main(){
-	int row= int.parse(stdin.readLineSync()!);
-	for(int i =1;i<=row;i++){
-		int num1=i;
-		int num2= row;
-		for(int j=1;j<=i;j++){
-			stdout.write("${num1} ");
-			num1+=num2;
-			num2--;
-		}
-		print("");
-	}
+        int row= int.parse(stdin.readLineSync()!);
+        int num1=1;
+        int num2=1;
+        for(int i=1;i<=row;i++){
+                for(int j=1;j<=i;j++){
+                        stdout.write("${num1} ");
+                        int num3=num1+ num2;
+                        num1=num2;
+                        num2=num3;
+                }
+                print("");
+        }
 }
